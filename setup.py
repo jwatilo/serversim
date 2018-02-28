c0a138895294d342ae3642d34dceb313735d277b
John Watilo <john@johnwatilo.com>
2018-02-28 17:59:51 -0600

    s2i configuration tweaks

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
-
-from os         import path
+from setuptools import find_packages
 from setuptools import setup
-
-def read( fname ):
-    return open( path.join( path.dirname( __file__ ), fname ) ).read()
 
 setup( name             = 'serversim'
      , version          = '1.0.0'
@@ -12,7 +7,8 @@ setup( name             = 'serversim'
      , author_email     = 'john@johnwatilo.com'
      , description      = 'A sample Python TCP server application that doesn\'t do much.'
      , keywords         = 'serversim'
-     , packages         = ['service', 'client', 'tests']
+     , packages         = find_packages() 
      , url              = 'http://github.com/jwatilo/serversim'
      , long_description = read( 'README.md' )
+     , install_requires = ['coverage']
      )
